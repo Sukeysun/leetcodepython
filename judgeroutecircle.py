e1bd997ceb74d90dd16a3672907668c6b9c48ce9
Sukeysun <578108683@qq.com>
2018-05-09 09:57:29 +0800

    Add files via upload

---
+++ judgeroutecircle.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 09:55:07 2018
+
+@author: sun_y
+"""
+
+"""
+Initially, there is a Robot at position (0, 0). Given a sequence of its moves, judge if this robot makes a circle, which means it moves back to the original place. 
+The move sequence is represented by a string. And each move is represent by a character. The valid robot moves are R (Right), L (Left), U (Up) and D (down). The output should be true or false representing whether the robot makes a circle. 
+Example 1:
+Input: "UD"
+Output: true
+
+Example 2:
+Input: "LL"
+Output: false
+"""
+class Solution:
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        if moves.count("U")==moves.count("D") and moves.count("L")==moves.count("r"):
+            return True
+        else:
+            return False
